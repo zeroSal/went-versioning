2c7252285e157f908ac59f8a2d288dc06c142f9c
Luca Saladino <luca.saladino@libraesva.com>
2026-05-13 10:36:26 +0200

    First implementation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zeroSal/wentversioning
+
+go 1.25.5
---
+++ versioning/specs.go
@@ -0,0 +1,31 @@
+package versioning
+
+type Specs struct {
+	version   string
+	channel   string
+	buildDate string
+}
+
+func NewSpecs(
+	version string,
+	channel string,
+	buildDate string,
+) *Specs {
+	return &Specs{
+		version:   version,
+		channel:   channel,
+		buildDate: buildDate,
+	}
+}
+
+func (specs *Specs) GetVersion() string {
+	return specs.version
+}
+
+func (specs *Specs) GetChannel() string {
+	return specs.channel
+}
+
+func (specs *Specs) GetBuildDate() string {
+	return specs.buildDate
+}
